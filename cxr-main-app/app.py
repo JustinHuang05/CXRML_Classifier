50a9f2514e1bbaac32d3b9b7e31d0d2a7e710a8b
JustinHuang05 <justin.sy.huang@gmail.com>
2025-01-07 12:43:38 -0500

    Cloud Run CI/CD Final Test (used act for previous CI/CD GitHub Actions testing)

--- cxr-main-app/app.py
+++ cxr-main-app/app.py
@@ -12,7 +12,7 @@ UPLOAD_FOLDER = 'uploads'
 TEST_DATA_FOLDER = 'cxr_test_data'
 EXTERNAL_TEST_DATA_FOLDER = 'external_test_data'
 
-ENDPOINT_URL = 'https://cxrmlclassifier-277681696511.us-central1.run.app/predict' 
+ENDPOINT_URL = 'https://cxrml-service-277681696511.us-central1.run.app/predict' 
 
 @app.route('/')
 def home_page():
